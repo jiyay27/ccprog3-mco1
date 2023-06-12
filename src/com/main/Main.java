9197044bfa880be7bbd7c463128f5e3be04977c6
John Joseph Donato <donatojohnjoseph@gmail.com>
2023-06-12 15:57:41 +0800

    commitidk

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -1,7 +1,19 @@
 package com.main;
 
+// import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int nChoice;
 
+        System.out.println();
+        System.out.println("\t MAIN MENU \t\n" +
+                "\n[1] Create a Vending Machine" +
+                "\n[2] Test a Vending Machine" +
+                "\n[3] Exit\n");
+        nChoice = scan.nextInt();
+
+        scan.close();
     }
 }
