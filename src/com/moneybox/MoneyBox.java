290339bfd0d5d444660d00fe34b04ea4efbf1dd2
jiyay27 <donatojohnjoseph@gmail.com>
2023-06-27 21:34:25 +0800

    update moneybox

--- src/com/moneybox/MoneyBox.java
+++ src/com/moneybox/MoneyBox.java
@@ -6,11 +6,11 @@ import com.money.Money;
 
 public class MoneyBox {
     private ArrayList<Money> nDenominations;
-    private ArrayList<Money> nAddedMoney;
+    private ArrayList<Money> nEarnings;
 
     public MoneyBox() {
         this.nDenominations = new ArrayList<Money>();
-        this.nAddedMoney = null;
+        this.nEarnings = new ArrayList<Money>();
 
         for (int i = 0; i < 10; i++) {
             this.nDenominations.add(new Money(200));
@@ -46,7 +46,17 @@ public class MoneyBox {
 
     public void addMoney(int nValue) {
         this.nDenominations.add(new Money(nValue));
-        this.nAddedMoney.add(new Money(nValue));
+        this.nEarnings.add(new Money(nValue));
+    }
+
+    public void insertMoney(int nInput) {
+        if (nInput == 500 || nInput == 200
+                || nInput == 100 || nInput == 50
+                || nInput == 20 || nInput == 10
+                || nInput == 5 || nInput == 1) {
+            addMoney(nInput);
+        } else
+            System.out.println("Money not accepted... \nPlease insert the right denomation...");
     }
 
     public void produceChange() {
