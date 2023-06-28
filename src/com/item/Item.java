74831bba480e834e754a3962948da798e2d20059
lexymochi <beaarcega28@gmail.com>
2023-06-29 07:14:13 +0800

    documentation for item class
    will update latur

--- src/com/item/Item.java
+++ src/com/item/Item.java
@@ -13,6 +13,12 @@ public class Item {
         this.nItemQuantity = nItemQuantity;
     }
 
+    /** 
+        checks if the item is still available to be sold
+        
+        @param nItemSold number of item/s to be checked
+        @return true if the item sold is less than the quantity provided and otherwise false
+    */
     private boolean isAllowSell(int nItemSold) {
         if (nItemSold <= this.nItemQuantity)
             return true;
@@ -20,20 +26,42 @@ public class Item {
             return false;
     }
 
+    /** 
+        handles item purchases wherein sold quantity will be deducted to the available one
+        
+        @param nItemSold number of item/s to be checked
+        @return the number of item/s sold
+    */
     public int buyItem(int nItemSold) {
         if (isAllowSell(nItemSold))
             this.nItemQuantity -= nItemSold;
         return nItemSold;
     }
 
+    /** 
+        sets the price of an item
+        
+        @param nItemPrice represents the new item price
+    */
     public void setItemPrice(int nItemPrice) {
         this.nItemPrice = nItemPrice;
     }
 
+    /** 
+        sets the quantity of an item
+        
+        @param nItemQuantity represents the new item quantity
+    */
     public void setItemQuantity(int nItemQuantity) {
         this.nItemQuantity = nItemQuantity;
     }
 
+    /** 
+        checks if restocking is possible for an item
+        
+        @param nQuantity quantity of items to be restocked
+        @return true if restocking is possible and false otherwise
+    */
     public boolean canRestock(int nQuantity) {
         if (this.getItemQuantity() < 15)
             if (nQuantity <= 15 - this.getItemQuantity()) {
@@ -42,22 +70,47 @@ public class Item {
         return false;
     }
 
+    /** 
+        used to increase item quantity in stock
+        
+        @param nQuantity represents the number of items to be restocked
+    */
     public void restockItem(int nQuantity) {
         this.setItemQuantity(this.getItemQuantity() + nQuantity);
     }
 
+    /** 
+        returns the value stored in the strItemName variable
+        
+        @return this.strItemName represents the item name stored
+    */
     public String getItemName() {
         return this.strItemName;
     }
 
+    /** 
+        returns the value stored in the nItemPrice variable
+        
+        @return this.nItemPrice represents the item price stored
+    */
     public int getItemPrice() {
         return this.nItemPrice;
     }
 
+    /** 
+        returns the value stored in the nItemCalories variable
+        
+        @return this.nItemCalories represents the item calorie/s stored
+    */
     public int getItemCalories() {
         return this.nItemCalories;
     }
 
+    /** 
+        returns the value stored in the nItemQuantity variable
+        
+        @return this.nItemQuantity represents the item quantity/ies stored
+    */
     public int getItemQuantity() {
         return this.nItemQuantity;
     }
--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -1,3 +1,13 @@
+/*********************************************************************************************************************
+This is to certify that this project is our own work, based on our personal efforts in studying and applying the 
+concepts learned. We have constructed the methods and their respective algorithms and corresponding code by 
+ourselves. The program was run, tested, and debugged by our own efforts. We further certify that we have not copied in 
+part or whole or otherwise plagiarized the work of other students and/or persons.
+                                                                        
+                                                                ARCEGA BEA ALEXIS ADJIE,        DLSU ID# 12209173
+                                                                DONATO JOHN JOSEPH DEL ROSARIO, DLSU ID# 12206482
+**********************************************************************************************************************/
+
 package com.main;
 
 import java.util.Scanner;
@@ -6,6 +16,7 @@ import com.item.Item;
 import com.itemslot.ItemSlot;
 import com.regularvendingmachine.RegularVendingMachine;
 
+// serves as a driver for the whole program
 public class Main {
     public static void main(String[] args) {
         int nChoice = -1;
