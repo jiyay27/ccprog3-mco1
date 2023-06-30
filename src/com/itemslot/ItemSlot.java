02a24e0aff7e681d60d80e5f26bee5b2718ebaea
lexymochi <beaarcega28@gmail.com>
2023-06-30 08:22:21 +0800

    Revert "docu"
    This reverts commit dcfd71c25a1fe6360db56483a42e3be0561b7bb1.

--- src/com/itemslot/ItemSlot.java
+++ src/com/itemslot/ItemSlot.java
@@ -2,38 +2,25 @@ package com.itemslot;
 
 import com.item.Item;
 
-/**
- * This is <code>ItemSlot</code> class which represents the itemslot object of the machine
- */
 public class ItemSlot {
     private int nSlotIndex;
     private Item itemOccupy;
 
-    /**
-     * This is the item constructor
-     * 
-     * @param nSlotIndex slot index
-     * @param itemOccupy item occupied
-     */
+    /** 
+        checks if the item is still available to be sold
+        
+        @param nItemSold number of item/s to be checked
+        @return true if the item sold is less than the quantity provided and otherwise false
+    */
     public ItemSlot(int nSlotIndex, Item itemOccupy) {
         this.nSlotIndex = nSlotIndex;
         this.itemOccupy = itemOccupy;
     }
 
-    /**
-     * Sets the occupied item
-     * 
-     * @param itemOccupy represents the new occupied item
-     */
     public void setItem(Item itemOccupy) {
         this.itemOccupy = itemOccupy;
     }
 
-    /**
-     * Checks the availability of an item using its quantity
-     * 
-     * @return true if the quantity is not equal to 0 and false otherwise
-     */
     public boolean getItemAvailability() {
         if (this.itemOccupy.getItemQuantity() != 0)
             return true;
@@ -41,28 +28,14 @@ public class ItemSlot {
             return false;
     }
 
-    /**
-     * Returns the value stored in the nSlotIndex variable
-     * 
-     * @return this.nSlotIndex represents index of the slot stored
-     */
     public int getSlotIndex() {
         return this.nSlotIndex;
     }
 
-    /**
-     * Returns the value stored in the itemOccupy variable
-     * 
-     * @return this.itemOccupy represents the item occupied
-     */
     public Item getItem() {
         return this.itemOccupy;
     }
 
-    /**
-     * This method displays the item occupied including the item name, price,
-     * calories, as well as the quantity
-     */
     public void listSlotInfo() {
         System.out.println();
         System.out.println("Item: \t\t" + this.itemOccupy.getItemName());
