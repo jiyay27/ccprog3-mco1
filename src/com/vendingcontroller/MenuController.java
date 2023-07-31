ba4321c46e8e36f1d0dc9d2d14d3ae771dda5125
lexymochi <beaarcega28@gmail.com>
2023-07-31 22:43:00 +0800

    try gui 1

---
+++ src/com/vendingcontroller/MenuController.java
@@ -0,0 +1,13 @@
+package com.vendingcontroller;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import com.vendingview.menugui.*;
+
+public class MenuController {
+    public static void main(String[] args) {
+        new MenuGui();
+
+    }
+}
--- src/com/vendingview/menugui/MenuGui.java
+++ src/com/vendingview/menugui/MenuGui.java
@@ -1,7 +1,39 @@
 package com.vendingview.menugui;
 
-
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
 
 public class MenuGui {
-    
+    private JFrame mainFrame;
+    private JPanel mainPanel; 
+
+    public MenuGui() {
+        this.mainFrame = new JFrame("Vending Machine Factory");
+        this.mainPanel = new JPanel();
+        this.mainPanel.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
+        this.mainPanel.setLayout(new GridLayout(5,3));
+
+        this.mainFrame.add(this.mainFrame, BorderLayout.CENTER);
+        this.mainFrame.pack();
+
+        this.mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //this.mainFrame.setLayout(new FlowLayout());
+        //this.mainFrame.setSize(500, 800);
+
+        this.mainFrame.setVisible(true);
+    }
+
+    private void initializeGreetingElements() {
+        JLabel menuName = new JLabel();
+        menuName.setText("Vending Machine Factory");
+    }
+
+    private void initializeAlignmentElements() {
+
+    }
+
+    private void initializeSwappingElements() {
+
+    }
 }
